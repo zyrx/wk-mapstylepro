7162b8a238d82227f8d3f2e5f9a45f493153ade9
Lech H. Conde <lech@zyrx.com.mx>
2014-01-16 19:12:04 -0600

    Update lazyloader.js

--- js/lazyloader.js
+++ js/lazyloader.js
@@ -1,2 +1,2 @@
 /* Copyright (C) Lech H. Conde */
-(function(){$widgetkit.lazyloaders.googlemaps=function(a,b){$widgetkit.load(WIDGETKIT_URL+"/widgets/map/styles/asesores/js/map.js").done(function(){a.googlemaps(b)})}})(jQuery);
+(function(){$widgetkit.lazyloaders.googlemaps=function(a,b){$widgetkit.load(WIDGETKIT_URL+"/widgets/map/styles/wk-mapstylepro/js/map.js").done(function(){a.googlemaps(b)})}})(jQuery);
